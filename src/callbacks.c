c96205771d48285efc5e465abe48f03f5cf32e39
oussamamouldi <oussama.mouldi@esprit.tn>
2023-11-15 19:26:57 +0100

    first commit

---
+++ src/callbacks.c
@@ -0,0 +1,154 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ajoutbutton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_rechbutton_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_suppbutton_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_modifbutton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonajoutfiche_clicked            (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonrech_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonsupp_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_buttonmod_clicked                   (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ajouter_clicked                     (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_utbuttonsupp_clicked                (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_utbuttonmod_clicked                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_gestut_clicked                      (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_gestetab_clicked                    (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ajouterdon_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_modifierdon_clicked                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_rechdonbutton_clicked               (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_suppdonbutton_clicked               (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/callbacks.h
@@ -0,0 +1,74 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button1_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ajoutbutton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rechbutton_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_suppbutton_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_modifbutton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonajoutfiche_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonrech_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonsupp_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonmod_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ajouter_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_utbuttonsupp_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_utbuttonmod_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_gestut_clicked                      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_gestetab_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ajouterdon_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_modifierdon_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rechdonbutton_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_suppdonbutton_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
